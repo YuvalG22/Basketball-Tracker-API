fa337e911253785039715366b59184ed30d737b4
Yuval Ganon <yuvalpay22@gmail.com>
2026-05-15 01:31:31 +0300

    pbp

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -221,7 +221,8 @@ export const getGameStats = async (req, res) => {
     'FT_MADE',
     'OPP_TWO_MADE',
     'OPP_THREE_MADE',
-    'OPP_FT_MADE'
+    'OPP_FT_MADE',
+    'PERIOD_END'
   )
   ORDER BY e.period ASC, e.clock_sec_remaining DESC, e.created_at ASC
   `,
